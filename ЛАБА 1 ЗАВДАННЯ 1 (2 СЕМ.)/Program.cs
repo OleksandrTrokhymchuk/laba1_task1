cff0826d3eeaa9d540f37bcdbd3a3b910d15cd0a
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2023-03-14 20:34:40 +0200

    laba1_task1
    laba1_task1

---
+++ ЛАБА 1 ЗАВДАННЯ 1 (2 СЕМ.)/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Xml.Linq;
+
+namespace laba
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> WordsInDictionary = new Dictionary<int, string>();
+            Console.WriteLine("Enter text: ");
+            string text = Console.ReadLine();
+
+            string[] arrayOfWordsFromText = text.Split(' ');
+
+            int index = 0;
+
+            while (index < arrayOfWordsFromText.Length)
+            {
+                WordsInDictionary.Add(index, arrayOfWordsFromText[index]);
+                index++;
+            }
+           
+
+            Console.WriteLine($"\nNumber of words in text = {arrayOfWordsFromText.Length}\n");
+
+            foreach (KeyValuePair<int, string> i in WordsInDictionary)
+            {
+                Console.WriteLine(i.Key + " " + i.Value);
+            }
+
+
+            Console.Write("\nEnter element: ");
+            int element = int.Parse(Console.ReadLine());
+
+            string direction;
+
+            do
+            {
+                Console.Write("\nEnter direction (back/forward): ");
+                direction = Console.ReadLine();
+
+                if (direction == "forward")
+                {
+                    for (index = element; index < arrayOfWordsFromText.Length; index++)
+                    {
+                        Console.WriteLine(arrayOfWordsFromText[index]);
+                    }
+                    break;
+                }
+
+                if (direction == "back")
+                {
+                    for (index = element; index >= 0; index--)
+                    {
+                        Console.WriteLine(arrayOfWordsFromText[index]);
+                    }
+                    break;
+                }
+            } while (direction != "forward" || direction != "back");
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
+
+
+
